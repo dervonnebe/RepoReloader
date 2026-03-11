90963e57bd18bd774076ecb9ea5589d30b6374ef
Jack F. <jackfeuchte@outlook.de>
2026-03-11 22:11:20 +0100

    Initialize RepoReloader project with Gradle setup and basic plugin structure

---
+++ src/main/java/top/jaxlabs/repoReloader/RepoReloader.java
@@ -0,0 +1,17 @@
+package top.jaxlabs.repoReloader;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RepoReloader extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
